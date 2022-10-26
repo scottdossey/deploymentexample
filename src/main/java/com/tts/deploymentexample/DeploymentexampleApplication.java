e3ee807d88a6285579d3f8c45975b58bd91e6ac9
sdossey <sdossey@gmail.com>
2022-10-26 08:46:31 -0500

    Initial commit

---
+++ src/main/java/com/tts/deploymentexample/DeploymentexampleApplication.java
@@ -0,0 +1,13 @@
+package com.tts.deploymentexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentexampleApplication.class, args);
+	}
+
+}
